198462ebcad10cddaa2d9128fc5a1ccfdc3146a4
Sahil Rajput <sahilrajput03@gmail.com>
2021-01-12 18:02:26 +0000

    Pre version commit. ~sahil

---
+++ index.js
@@ -0,0 +1,38 @@
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
+export const PageList = ({list}) => {
+  const Breaker = Array(20)
+    .fill()
+    .map((e, k) => <br key={k} />);
+  return (
+    <>
+      <Router
+        children={
+          <>
+            <Switch
+              children={list.map(([path, component]) => (
+                <Route key={path} path={path} children={component} />
+              ))}
+            />
+
+            <div>
+              {Breaker}
+              <br />
+              <h2>Visit apps:</h2>
+              {list.map(([path]) => (
+                <div key={path}>
+                  <Link to={path}>{path}</Link>
+                  <br />
+                </div>
+              ))}
+            </div>
+          </>
+        }
+      />
+    </>
+  );
+};
+
+const Pages = ({children}) => {
+  return <Router children={<Switch children={children} />} />;
+};
